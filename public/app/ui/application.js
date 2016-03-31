85c99e3c1b51370c144b45b4b6b781f65df0b2bc
Vincent Trumpff <vincent@stoic.com>
2016-03-31 22:19:43 +0200

    main layout

--- public/app/ui/application.js
+++ public/app/ui/application.js
@@ -2,15 +2,43 @@
 
 import React from 'react';
 
-import {AppBar} from 'material-ui';
-
+import AppBar from 'material-ui/lib/app-bar';
+import Tabs from 'material-ui/lib/tabs/tabs';
+import Tab from 'material-ui/lib/tabs/tab';
 import HelloWorld from './hello-world-component';
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+};
 
 class Application extends React.Component {
   render() { return (
     <div>
-      <AppBar/>
-      <HelloWorld/>
+      <AppBar title="MyLife Home Studio" showMenuIconButton={false}/>
+      <Tabs>
+        <Tab label="Item One">
+          <div>
+            <h2 style={styles.headline}>Tab One</h2>
+            <p>
+              This is an example tab.
+            </p>
+            <HelloWorld/>
+          </div>
+        </Tab>
+        <Tab label="Item Two" >
+          <div>
+            <h2 style={styles.headline}>Tab Two</h2>
+            <p>
+              This is another example tab.
+            </p>
+          </div>
+        </Tab>
+      </Tabs>
     </div>
   ); }
 }
