69349707a0634d480d70f5e006f57bbd00d32b58
Vincent Trumpff <vincent.tr@gmail.com>
2016-04-03 21:48:00 +0200

    toolbar

--- public/app/ui/main-toolbar.js
+++ public/app/ui/main-toolbar.js
@@ -6,35 +6,52 @@ import Toolbar from 'material-ui/lib/toolbar/toolbar';
 import ToolbarGroup from 'material-ui/lib/toolbar/toolbar-group';
 import ToolbarSeparator from 'material-ui/lib/toolbar/toolbar-separator';
 import ToolbarTitle from 'material-ui/lib/toolbar/toolbar-title';
-
-import FontIcon from 'material-ui/lib/font-icon';
-import RaisedButton from 'material-ui/lib/raised-button';
-import IconMenu from 'material-ui/lib/menus/icon-menu';
 import IconButton from 'material-ui/lib/icon-button';
-import NavigationExpandMoreIcon from 'material-ui/lib/svg-icons/navigation/expand-more';
-import MenuItem from 'material-ui/lib/menus/menu-item';
+
+import FileCreateNewFolder from 'material-ui/lib/svg-icons/file/create-new-folder';
+import FileFolderOpen from 'material-ui/lib/svg-icons/file/folder-open';
+import ActionOpenInBrowser from 'material-ui/lib/svg-icons/action/open-in-browser';
+import ContentSave from 'material-ui/lib/svg-icons/content/save';
 
 class MainToolbar extends React.Component {
   render() { return (
     <Toolbar>
-      <ToolbarGroup firstChild={true} float="left">
-        <ToolbarTitle text="Zouave" />
-      </ToolbarGroup>
-      <ToolbarGroup float="right">
-        <ToolbarTitle text="Options" />
-        <FontIcon className="muidocs-icon-custom-sort" />
-        <IconMenu
-          iconButtonElement={
-            <IconButton touch={true}>
-              <NavigationExpandMoreIcon />
-            </IconButton>
-          }
-        >
-          <MenuItem primaryText="Download" />
-          <MenuItem primaryText="More Info" />
-        </IconMenu>
+      <ToolbarGroup float="left">
+        <ToolbarTitle text="vpanel" />
+        <IconButton tooltip="new">
+          <FileCreateNewFolder />
+        </IconButton>
+        <IconButton tooltip="open online">
+          <FileFolderOpen />
+        </IconButton>
+        <IconButton tooltip="open from file">
+          <ActionOpenInBrowser />
+        </IconButton>
+
         <ToolbarSeparator />
-        <RaisedButton label="Create Broadcast" primary={true} />
+
+        <ToolbarTitle text="ui"/>
+        <IconButton tooltip="new">
+          <FileCreateNewFolder />
+        </IconButton>
+        <IconButton tooltip="open online">
+          <FileFolderOpen />
+        </IconButton>
+        <IconButton tooltip="open from file">
+          <ActionOpenInBrowser />
+        </IconButton>
+
+        <ToolbarSeparator />
+
+        <IconButton tooltip="save all">
+          <ContentSave />
+        </IconButton>
+        <IconButton tooltip="save">
+          <ContentSave />
+        </IconButton>
+        <IconButton tooltip="save as">
+          <ContentSave />
+        </IconButton>
       </ToolbarGroup>
     </Toolbar>
   ); }
