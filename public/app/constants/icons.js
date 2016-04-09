928de067e77a7942ac0732c62a21421e38264c6c
Vincent Trumpff <vincent@stoic.com>
2016-04-09 21:54:48 +0200

    icons

---
+++ public/app/constants/icons.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import HardwareMemory from 'material-ui/lib/svg-icons/hardware/memory';
+import HardwareRouter from 'material-ui/lib/svg-icons/hardware/router';
+import NotificationPersonalVideo from 'material-ui/lib/svg-icons/notification/personal-video';
+import ResourcesImportContacts from 'material-ui/lib/svg-icons/resources/import-contacts';
+import SocialPublic from 'material-ui/lib/svg-icons/social/public';
+
+export default = {
+  Entity=HardwareRouter,
+  ENTITY_CORE=SocialPublic,
+  ENTITY_RESOURCES=ResourcesImportContacts,
+  ENTITY_UI=NotificationPersonalVideo,
+
+  DRIVER=HardwareMemory
+};
