cc50ca48ab423e1fb46da7701a49407b684cd09b
Vincent Trumpff <vincent@stoic.com>
2016-04-11 11:29:06 +0200

    moved icons

--- public/app/components/base/icons.js
+++ public/app/components/base/icons.js
@@ -3,14 +3,14 @@
 import HardwareMemory from 'material-ui/lib/svg-icons/hardware/memory';
 import HardwareRouter from 'material-ui/lib/svg-icons/hardware/router';
 import NotificationPersonalVideo from 'material-ui/lib/svg-icons/notification/personal-video';
-import ResourcesImportContacts from 'material-ui/lib/svg-icons/resources/import-contacts';
+import CommunicationImportContacts from 'material-ui/lib/svg-icons/communication/import-contacts';
 import SocialPublic from 'material-ui/lib/svg-icons/social/public';
 
-export default = {
-  Entity=HardwareRouter,
-  ENTITY_CORE=SocialPublic,
-  ENTITY_RESOURCES=ResourcesImportContacts,
-  ENTITY_UI=NotificationPersonalVideo,
+export default {
+  Entity          : HardwareRouter,
+  EntityCore      : SocialPublic,
+  EntityResources : CommunicationImportContacts,
+  EntityUi        : NotificationPersonalVideo,
 
-  DRIVER=HardwareMemory
+  PluginDriver    : HardwareMemory
 };
--- public/app/components/base/index.js
+++ public/app/components/base/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+import icons from './icons';
 import SelectableList from './selectable-list';
 import SelectableListItem from './selectable-list-item';
 
 export default {
   SelectableList,
-  SelectableListItem
+  SelectableListItem,
+  icons
 };
