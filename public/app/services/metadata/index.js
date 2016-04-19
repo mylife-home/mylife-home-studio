c2ded812c4fd150e76ea9efc04a51fa9332503f4
Vincent Trumpff <vincent@stoic.com>
2016-04-19 23:57:26 +0200

    parseClass

--- public/app/services/metadata/index.js
+++ public/app/services/metadata/index.js
@@ -24,6 +24,37 @@ class Metadata {
     return TypeFactory.parse(value);
   }
 
+  parseClass(value) {
+    console.log(value);
+    const ret = {
+      attributes: [],
+      actions: []
+    };
+
+    const members = value.split('|');
+    for(const member of members) {
+      const parts = member.substr(1).split(',');
+      const name = parts[0];
+      const types = parts.splice(1).map(this.parseType.bind(this));
+      switch(member[0]) {
+      case '=': // attribute
+        ret.attributes.push({
+          name,
+          type: types[0]
+        });
+        break;
+      case '.': // action
+        ret.actions.push({
+          name,
+          types
+        });
+        break;
+      }
+    }
+
+    return ret;
+  }
+
   createRange(min, max) {
     return TypeFactory.createRange(min, max);
   }
