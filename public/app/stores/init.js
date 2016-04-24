65bfe344c1dd387150b32d98376eaafafe9c979a
Vincent Trumpff <vincent@stoic.com>
2016-04-24 22:16:31 +0200

    store init enhancement

---
+++ public/app/stores/init.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import OnlineStore  from './online-store'; // import to force init
+import ErrorStore   from './error-store'; // import to force init
+import ProjectStore from './project-store'; // import to force init
