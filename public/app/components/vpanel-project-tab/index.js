052229d897e695e024dad4458d3b47954efe2eb2
Vincent Trumpff <vincent@stoic.com>
2016-04-25 23:20:52 +0200

    project tabs skeleton

--- public/app/components/vpanel-project-tab/index.js
+++ public/app/components/vpanel-project-tab/index.js
@@ -4,8 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as mui from 'material-ui';
 import * as bs from 'react-bootstrap';
+import base from '../base/index';
 
 import ProjectActionCreators from '../../actions/project-action-creators';
+
+import tabStyles from '../base/tab-styles';
 
 class VPanelProjectTab extends React.Component {
 
@@ -17,13 +20,33 @@ class VPanelProjectTab extends React.Component {
     const project = this.props.project;
 
     return (
-      <div>
-        <h2>{project.name}</h2>
-        <p>
-          <mui.FlatButton onClick={() => ProjectActionCreators.close(project)}>Close</mui.FlatButton>
-          TODO
-        </p>
-      </div>
+      <bs.Grid fluid={true} style={Object.assign({}, tabStyles.fullHeight)}>
+        <bs.Row style={tabStyles.fullHeight}>
+          <bs.Col sm={2} style={Object.assign({}, tabStyles.noPadding, tabStyles.fullHeight)}>
+            <mui.Paper style={Object.assign({}, tabStyles.scrollable, tabStyles.fullHeight)}>
+              TOOLBOX
+            </mui.Paper>
+          </bs.Col>
+          <bs.Col sm={8} style={Object.assign({}, tabStyles.noPadding, tabStyles.scrollable, tabStyles.fullHeight)}>
+            <div>
+              <base.DetailsTitle
+                center={project.name}
+                left={<base.icons.tabs.VPanel />}
+                right={
+                  <mui.IconButton onClick={() => ProjectActionCreators.close(project)}>
+                    <base.icons.actions.Close />
+                  </mui.IconButton>
+                }/>
+              TODO
+            </div>
+          </bs.Col>
+          <bs.Col sm={2} style={Object.assign({}, tabStyles.noPadding, tabStyles.fullHeight)}>
+            <mui.Paper style={Object.assign({}, tabStyles.scrollable, tabStyles.fullHeight)}>
+              PROPERTIES
+            </mui.Paper>
+          </bs.Col>
+        </bs.Row>
+      </bs.Grid>
     );
   }
 }
