a95b93d56c98ebb4f13824057ed0f46af652f0ce
Vincent Trumpff <vincent@stoic.com>
2016-05-25 22:29:05 +0200

    cleanup

--- public/app/services/projects/project.js
+++ public/app/services/projects/project.js
@@ -1,9 +1,6 @@
 'use strict';
 
 import uuid from 'uuid';
-import debugLib from 'debug';
-
-const debug = debugLib('mylife:home:studio:services:projects');
 
 class Project {
   constructor(type, data) {
