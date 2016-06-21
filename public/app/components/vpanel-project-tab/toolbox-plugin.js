c774fbe6de2f51eee4c3d543ffb9ea263d42ec9d
Vincent Trumpff <vincent@stoic.com>
2016-06-21 10:22:46 +0200

    toolbox design

--- public/app/components/vpanel-project-tab/toolbox-plugin.js
+++ public/app/components/vpanel-project-tab/toolbox-plugin.js
@@ -7,7 +7,28 @@ import * as bs from 'react-bootstrap';
 import * as dnd from 'react-dnd';
 import base from '../base/index';
 
+import Facade from '../../services/facade';
+
 import AppConstants from '../../constants/app-constants';
+
+const styles = {
+  iconContainer: {
+    float  : 'left',
+    width  : '32px',
+    height : '32px'
+  },
+  icon: {
+    textAlign     : 'center',
+    height        : '32px',
+    lineHeight    : '32px',
+    verticalAlign : 'middle'
+  },
+  textContainer: {
+    lineHeight : '32px',
+    margin     : '0px',
+    marginLeft : '40px'
+  }
+}
 
 class ToolboxPlugin extends React.Component {
 
@@ -15,15 +36,58 @@ class ToolboxPlugin extends React.Component {
     super(props);
   }
 
+  renderIcon() {
+    const { connectDragPreview, plugin } = this.props;
+
+    switch(plugin.usage) {
+    case Facade.metadata.pluginUsage.driver:
+      return (
+        <base.TooltipContainer tooltip="Hardware driver" tooltipPosition="bottom-right">
+          {connectDragPreview(
+            <div style={styles.iconContainer}>
+              <base.icons.PluginDriver style={styles.icon} />
+            </div>
+          )}
+        </base.TooltipContainer>
+      );
+
+    case Facade.metadata.pluginUsage.vpanel:
+      return (
+        <base.TooltipContainer tooltip="Virtual panel" tooltipPosition="bottom-right">
+          {connectDragPreview(
+            <div style={styles.iconContainer}>
+              <base.icons.PluginVPanel style={styles.icon} />
+            </div>
+          )}
+        </base.TooltipContainer>
+      );
+
+    case Facade.metadata.pluginUsage.ui:
+      return (
+        <base.TooltipContainer tooltip="UI" tooltipPosition="bottom-right">
+          {connectDragPreview(
+            <div style={styles.iconContainer}>
+              <base.icons.PluginUi style={styles.icon} />
+            </div>
+          )}
+        </base.TooltipContainer>
+      );
+
+    default:
+      return null;
+    }
+  }
+
   render() {
-    const { connectDragSource, isDragging, plugin } = this.props;
+    const { connectDragSource, connectDragPreview, isDragging, plugin } = this.props;
 
     return connectDragSource(
       <div style={{
         opacity: isDragging ? 0.5 : 1,
         cursor: 'move'
       }}>
-        {plugin.library + ':' + plugin.type}
+        {this.renderIcon()}
+        <div style={styles.textContainer}>{plugin.library + ':' + plugin.type}</div>
       </div>
     );
   }
@@ -32,6 +96,7 @@ class ToolboxPlugin extends React.Component {
 ToolboxPlugin.propTypes = {
   plugin: React.PropTypes.object.isRequired,
   connectDragSource: React.PropTypes.func.isRequired,
+  connectDragPreview: React.PropTypes.func.isRequired,
   isDragging: React.PropTypes.bool.isRequired
 };
 
@@ -49,6 +114,7 @@ const pluginSource = {
 function collect(connect, monitor) {
   return {
     connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
     isDragging: monitor.isDragging()
   }
 }
