122886c62190d92a3b72248be7442a3a3d300070
Vincent Trumpff <vincent@stoic.com>
2016-07-12 14:40:10 +0200

    a-start first draft

--- public/app/components/vpanel-project-tab/link-helper.js
+++ public/app/components/vpanel-project-tab/link-helper.js
@@ -91,8 +91,26 @@ function componentMeasureMember(uiComponent, name) {
 
 function rebuild(project, projectState) {
   const linkData = data(projectState);
-  linkData.ObstacleGrid = buildObstacleGrid(linkData.measures);
-  // TODO
+  const measures = linkData.measures;
+  const obstacleGrid = buildObstacleGrid(measures);
+  const bindingPaths = new Map();
+  linkData.bindingPaths = bindingPaths;
+
+  for(const component of project.components) {
+    for(const binding of component.bindings) {
+      const startMeasure = measures.get(binding.remote.id);
+      const start = startMeasure && startMeasure[binding.remote_attribute];
+      const endMeasure = measures.get(binding.local.id);
+      const end = endMeasure && endMeasure[binding.local_action];
+      if(!start || !end) { continue; }
+
+      const path = findPath(obstacleGrid, convertAnchorToGrid(start), convertAnchorToGrid(end));
+      if(!path) { continue; } // TODO: fallback
+
+      console.log(binding, path);
+      bindingPaths.set(binding, path);
+    }
+  }
 
   ++linkData.version;
   ProjectActionCreators.stateRefresh(project);
@@ -108,7 +126,7 @@ function buildObstacleGrid(measures) {
   }
 
   for(const measure of measures.values()) {
-    const dim = convertToGrid(measure.__dim);
+    const dim = convertRectToGrid(measure.__dim);
     for(let x = dim.left; x <= dim.right; ++x) {
       for(let y=dim.top; y <= dim.bottom; ++y) {
         grid[x][y] = true;
@@ -119,7 +137,7 @@ function buildObstacleGrid(measures) {
   return grid;
 }
 
-function convertToGrid(rect) {
+function convertRectToGrid(rect) {
   const ret = {
     top:    Math.floor(rect.top   / GRID_SIZE),
     left:   Math.floor(rect.left  / GRID_SIZE),
@@ -129,4 +147,92 @@ function convertToGrid(rect) {
   ret.width = ret.right - ret.left;
   ret.height = ret.bottom - ret.top;
   return ret;
+}
+
+function convertAnchorToGrid(anchor) {
+  const y = Math.round(anchor.left.y / GRID_SIZE);
+  return {
+    left: {
+      x: Math.floor(anchor.left.x / GRID_SIZE),
+      y
+    },
+    right: {
+      x: Math.ceil(anchor.right.x / GRID_SIZE),
+      y
+    }
+  };
+}
+
+function findPath(obstacleGrid, start, end) {
+
+  // TODO: better
+  let startPoint;
+  if(isPointFree(obstacleGrid, start.left)) {
+    startPoint = start.left;
+  } else if(isPointFree(obstacleGrid, start.right)) {
+    startPoint = start.right;
+  } else {
+    return null; // no start point
+  }
+
+  // TODO: better
+  let endPoint;
+  if(isPointFree(obstacleGrid, end.left)) {
+    endPoint = end.left;
+  } else if(isPointFree(obstacleGrid, end.right)) {
+    endPoint = end.right;
+  } else {
+    return null; // no end point
+  }
+
+  const ret = aStar({
+    start: startPoint,
+    isEnd: pointEquals.bind(null, endPoint),
+    neighbor: neighbors.bind(null, obstacleGrid),
+    distance: rectilinearDistance,
+    heuristic: rectilinearDistance.bind(null, endPoint),
+    hash: pointHash,
+    //timeout
+  });
+
+  return ret.status === 'success' && ret.path;
+}
+
+function rectilinearDistance(a, b) {
+  const dx = b.x - a.x;
+  const dy = b.y - a.y;
+  return Math.abs(dx) + Math.abs(dy);
+};
+
+function isPointFree(obstacleGrid, point) {
+  return !obstacleGrid[point.x][point.y];
+}
+
+function pointEquals(p1, p2) {
+  return p1.x === p2.x && p1.y === p2.y;
+}
+
+function pointHash(point) {
+  return `${point.x}:${point.y}`;
+}
+
+function neighbors(obstacleGrid, point) {
+  const ret = [];
+  testAndAddShiftedPoint(-1, 0);
+  testAndAddShiftedPoint(1, 0);
+  testAndAddShiftedPoint(0, -1);
+  testAndAddShiftedPoint(0, 1);
+  return ret;
+
+  function testAndAddShiftedPoint(x, y) {
+    const newPoint = {
+      x: point.x + x,
+      y: point.y + y
+    };
+    if(x < 0 || x >= GRID_ROWS) { return; }
+    if(y < 0 || y >= GRID_COLS) { return; }
+    if(!isPointFree(obstacleGrid, newPoint)) { return; }
+
+    ret.push(newPoint);
+  }
 }
