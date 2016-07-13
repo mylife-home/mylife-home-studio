249e96066eed35b82a86858c4ba0ec56b0e9065b
Vincent Trumpff <vincent@stoic.com>
2016-07-13 19:20:32 +0200

    properties title

--- public/app/components/vpanel-project-tab/properties.js
+++ public/app/components/vpanel-project-tab/properties.js
@@ -10,13 +10,50 @@ import ProjectStore from '../../stores/project-store';
 import ProjectStateStore from '../../stores/project-state-store';
 import ProjectActionCreators from '../../actions/project-action-creators';
 
+function getStyles(props, state) {
+  const { baseTheme } = state.muiTheme;
+
+  return {
+    titleContainer: {
+      background : baseTheme.palette.primary3Color,
+      color      : baseTheme.palette.textColor,
+    },
+    titleText: {
+      whiteSpace    : 'nowrap',
+      overflow      : 'hidden',
+      textOverflow  : 'ellipsis',
+      margin        : 0,
+      paddingTop    : 2,
+      paddingLeft   : 8,
+      paddingRight  : 8,
+      letterSpacing : 0,
+      fontSize      : 16,
+      fontWeight    : 'normal',
+      height        : '25px',
+      lineHeight    : '25px'
+    },
+    titleIconContainer: {
+      float  : 'left',
+      width  : '25px',
+      height : '25px'
+    },
+    titleIcon: {
+      textAlign     : 'center',
+      height        : '25px',
+      lineHeight    : '25px',
+      verticalAlign : 'middle'
+    },
+  };
+}
+
 class Properties extends React.Component {
 
-  constructor(props) {
+  constructor(props, context) {
     super(props);
 
     this.state = {
-      selection: null
+      selection: null,
+      muiTheme: context.muiTheme || muiStyles.getMuiTheme()
     };
   }
 
@@ -38,37 +75,83 @@ class Properties extends React.Component {
     });
   }
 
-  renderTitle() {
-    const { selection } = this.state;
+  renderTitle(Icon, text) {
+    const styles = getStyles(this.props, this.state);
+    return (
+      <div style={styles.titleContainer}>
+        <div style={styles.titleIconContainer}>
+          <Icon color={styles.titleContainer.color} style={styles.titleIcon} />
+        </div>
+        <div style={styles.titleText}>
+          {text}
+        </div>
+      </div>
+    );
+  }
 
-    if(!selection) {
-      return (<div>PROJECT</div>);
-    }
+  renderComponent(component) {
+    return (
+      <div>
+        {this.renderTitle(base.icons.Component, component.id)}
+      </div>
+    );
+  }
 
-    switch(selection.type) {
-    case 'component':
-      return (<div>COMPONENT {selection.id}</div>);
+  renderBinding(binding) {
+    const key = `${binding.remote.id}:${binding.remote_attribute} -> ${binding.local.id}:${binding.local_action}`;
+    return (
+      <div>
+        {this.renderTitle(base.icons.Binding, key)}
+      </div>
+    );
+  }
 
-    case 'binding':
-      return (<div>BINDING</div>);
-    }
+  renderProject(project) {
+    return (
+      <div>
+        {this.renderTitle(base.icons.tabs.VPanel, 'Project')}
+      </div>
+    );
+  }
+
+  findComponent(project, componentId) {
+    return project.components.find(c => c.id === componentId);
   }
 
   render() {
     const { project } = this.props;
     const { selection } = this.state;
 
-    return (
-      <div>
-        PROPERTIES
-        {this.renderTitle()}
-      </div>
-    );
+    switch(selection && selection.type) {
+      case 'component': {
+        const component = this.findComponent(project, selection.id);
+        return this.renderComponent(component);
+      }
+
+      case 'binding': {
+        const component = this.findComponent(project, selection.localId);
+        const binding = component.bindings.find(b =>
+          b.remote.id === selection.remoteId &&
+          b.remote_attribute === selection.remoteAttribute &&
+          b.local_action === selection.localAction);
+        return this.renderBinding(binding);
+      }
+    }
+
+    return this.renderProject(project);
   }
 }
 
 Properties.propTypes = {
   project: React.PropTypes.object.isRequired,
+};
+
+Properties.contextTypes = {
+  muiTheme: React.PropTypes.object
+};
+
+Properties.childContextTypes = {
+  muiTheme: React.PropTypes.object
 };
 
 export default Properties;
