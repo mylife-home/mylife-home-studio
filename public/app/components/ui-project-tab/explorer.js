f86c0a8348989f9a8b0d0095d6f11d0bdc1f95ff
Vincent Trumpff <vincent@stoic.com>
2016-07-20 13:24:02 +0200

    explorer style wip (miss full height)

--- public/app/components/ui-project-tab/explorer.js
+++ public/app/components/ui-project-tab/explorer.js
@@ -8,6 +8,14 @@ import base from '../base/index';
 
 import Toolbar from './toolbar';
 
+import tabStyles from '../base/tab-styles';
+
+const styles = {
+  mainList: {
+    height: 'calc(100% - 74px)'
+  }
+};
+
 class Explorer extends React.Component {
 
   constructor(props) {
@@ -18,8 +26,13 @@ class Explorer extends React.Component {
     const project = this.props.project;
 
     return (
-      <div>
-        EXPLORER
+      <div style={Object.assign({}, tabStyles.fullHeight)}>
+        <mui.List style={Object.assign({}, tabStyles.scrollable, styles.mainList)}>
+          <mui.ListItem key={'project'} primaryText={'Project'} leftIcon={<base.icons.tabs.Ui />} />
+          <mui.ListItem key={'components'} primaryText={'Components'} leftIcon={<base.icons.Component />} />
+          <mui.ListItem key={'images'} primaryText={'Images'} leftIcon={<base.icons.UiImage />} />
+          <mui.ListItem key={'windows'} primaryText={'Windows'} leftIcon={<base.icons.UiWindow />} />
+        </mui.List>
         <Toolbar project={project} />
       </div>
     );
