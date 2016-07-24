3e9c975fc9cb12b4c147e9f311ddb602b15d51db
Vincent Trumpff <vincent@stoic.com>
2016-07-24 16:58:55 +0200

    activeContent

--- public/app/components/ui-project-tab/explorer.js
+++ public/app/components/ui-project-tab/explorer.js
@@ -6,6 +6,10 @@ import * as mui from 'material-ui';
 import * as bs from 'react-bootstrap';
 import base from '../base/index';
 
+import ProjectStore from '../../stores/project-store';
+import ProjectStateStore from '../../stores/project-state-store';
+import ProjectActionCreators from '../../actions/project-action-creators';
+
 import Toolbar from './toolbar';
 
 import tabStyles from '../base/tab-styles';
@@ -20,13 +24,32 @@ class Explorer extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.boundHandleStoreChange = this.handleStoreChange.bind(this);
+  }
+
+  componentDidMount() {
+    ProjectStore.addChangeListener(this.boundHandleStoreChange);
+  }
+
+  componentWillUnmount() {
+    ProjectStore.removeChangeListener(this.boundHandleStoreChange);
+  }
+
+  handleStoreChange() {
+    const project = this.props.project;
+    let projectVersion = project && project.version;
+    this.setState({ projectVersion });
   }
 
   renderComponents() {
     const project = this.props.project;
 
     return project.components.map(comp => (
-      <mui.ListItem key={`component:${comp.id}`} primaryText={comp.id} leftIcon={<base.icons.Component />} />
+      <mui.ListItem key={`component:${comp.id}`}
+                    primaryText={comp.id}
+                    leftIcon={<base.icons.Component />}
+                    onClick={this.select.bind(this, { type: 'component', id: comp.id})} />
     ));
   }
 
@@ -34,7 +57,10 @@ class Explorer extends React.Component {
     const project = this.props.project;
 
     return project.images.map(img => (
-      <mui.ListItem key={`image:${img.uid}`} primaryText={img.id} leftIcon={<base.icons.UiImage />} />
+      <mui.ListItem key={`image:${img.uid}`}
+                    primaryText={img.id}
+                    leftIcon={<base.icons.UiImage />}
+                    onClick={this.select.bind(this, { type: 'image', uid: img.uid})} />
     ));
   }
 
@@ -42,8 +68,18 @@ class Explorer extends React.Component {
     const project = this.props.project;
 
     return project.windows.map(window => (
-      <mui.ListItem key={`window:${window.uid}`} primaryText={window.id} leftIcon={<base.icons.UiWindow />} />
+      <mui.ListItem key={`window:${window.uid}`}
+                    primaryText={window.id}
+                    leftIcon={<base.icons.UiWindow />}
+                    onClick={this.select.bind(this, { type: 'window', uid: window.uid})} />
     ));
+  }
+
+  select(data) {
+    const project = this.props.project;
+    const state = ProjectStateStore.getProjectState(project);
+    state.activeContent = data;
+    ProjectActionCreators.stateRefresh(project);
   }
 
   render() {
@@ -55,7 +91,7 @@ class Explorer extends React.Component {
           <mui.ListItem key={'project'}
                         primaryText={'Project'}
                         leftIcon={<base.icons.tabs.Ui />}
-                        onClick={() => console.log('project')} />
+                        onClick={this.select.bind(this, null)} />
           <mui.ListItem key={'components'}
                         primaryText={'Components'}
                         leftIcon={<base.icons.Component />}
