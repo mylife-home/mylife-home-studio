f76b361deac47753b610f1f5d827d6a4a7e23f05
Vincent Trumpff <vincent@stoic.com>
2016-07-24 17:33:49 +0200

    canvas skeleton

--- public/app/components/ui-project-tab/canvas.js
+++ public/app/components/ui-project-tab/canvas.js
@@ -6,21 +6,74 @@ import * as mui from 'material-ui';
 import * as bs from 'react-bootstrap';
 import base from '../base/index';
 
+import ProjectStore from '../../stores/project-store';
+import ProjectStateStore from '../../stores/project-state-store';
+
 class Canvas extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = { };
+
+    this.boundHandleStoreChange = this.handleStoreChange.bind(this);
+  }
+
+  componentDidMount() {
+    ProjectStore.addChangeListener(this.boundHandleStoreChange);
+    ProjectStateStore.addChangeListener(this.boundHandleStoreChange);
+  }
+
+  componentWillUnmount() {
+    ProjectStore.removeChangeListener(this.boundHandleStoreChange);
+    ProjectStateStore.removeChangeListener(this.boundHandleStoreChange);
+  }
+
+  handleStoreChange() {
+    const project = this.props.project;
+    const projectVersion = project && project.version;
+    const state = ProjectStateStore.getProjectState(project);
+    const activeContent = state.activeContent;
+    this.setState({ projectVersion, activeContent });
+  }
+
+  renderProject(project) {
+    return <div>project</div>;
+  }
+
+  renderComponent(component) {
+    return <div>component</div>;
+  }
+
+  renderImage(image) {
+    return <div>image</div>;
+  }
+
+  renderWindow(window) {
+    return <div>window</div>;
   }
 
   render() {
     const project = this.props.project;
+    const activeContent = this.state.activeContent;
 
-    return (
-      <div>
-        CANVAS
-        {JSON.stringify(project.raw)}
-      </div>
-    );
+    if(activeContent) {
+      switch(activeContent.type) {
+        case 'component':
+          const component = project.components.find(comp => comp.uid === activeContent.uid);
+          return this.renderComponent(component);
+
+        case 'image':
+          const image = project.images.find(img => img.uid === activeContent.uid);
+          return this.renderImage(image);
+
+        case 'window':
+          const window = project.windows.find(wnd => wnd.uid === activeContent.uid);
+          return this.renderWindow(window);
+      }
+    }
+
+    return this.renderProject(project);
   }
 }
 
