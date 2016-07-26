7ef8389bdaf73379223181b665b0f7d5f521df14
Vincent Trumpff <vincent@stoic.com>
2016-07-26 11:32:25 +0200

    properly handle size changes

--- public/app/components/ui-project-tab/properties-image.js
+++ public/app/components/ui-project-tab/properties-image.js
@@ -18,6 +18,17 @@ class PropertiesImage extends React.Component {
     this.state = { };
 
     const image = this.props.image;
+    this.calculateSize(image);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ size : null });
+
+    const image = nextProps.image;
+    this.calculateSize(image);
+  }
+
+  calculateSize(image) {
     if(image && image.content) {
       base.utils.imageSize(image.content, (err, size) => {
         this.setState({ size });
