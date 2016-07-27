2633666dffcdb16ceeecd415150365b6a565e36e
Vincent Trumpff <vincent@stoic.com>
2016-07-27 21:10:57 +0200

    canvas-window skeleton

--- public/app/components/ui-project-tab/canvas-window.js
+++ public/app/components/ui-project-tab/canvas-window.js
@@ -4,34 +4,103 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as mui from 'material-ui';
 import * as bs from 'react-bootstrap';
-import rr from 'react-resizable';
+import ResizableBox from 'react-resizable-box';
+import { throttle, debounce } from 'throttle-debounce';
 import base from '../base/index';
 import commonStyles from './canvas-styles';
 
-const styles = Object.assign({
-}, commonStyles);
+import ProjectStore from '../../stores/project-store';
+import ProjectStateStore from '../../stores/project-state-store';
+import ProjectActionCreators from '../../actions/project-action-creators';
+
+function getStyles(props, state) {
+  const { muiTheme, isSelected } = state;
+  const { baseTheme } = muiTheme;
+
+  const backColor = (isSelected ? baseTheme.palette.primary1Color : baseTheme.palette.primary3Color);
+  const foreColor = (isSelected ? baseTheme.palette.alternateTextColor : baseTheme.palette.textColor);
+
+  return Object.assign({
+    window: {
+      height: '100%',
+      border: '1px solid ' + backColor
+    }
+  }, commonStyles);
+}
 
 class CanvasWindow extends React.Component {
 
-  constructor(props) {
-    super(props);
+  constructor(props, context) {
+    super(props, context);
+
+    const { project, window } = this.props;
+    const projectState = ProjectStateStore.getProjectState(project);
+
+    this.state = {
+      isSelected: projectState.selection && projectState.selection.type === 'window' && projectState.selection.uid === window.uid,
+      lastUpdate: props.project.lastUpdate,
+      muiTheme: context.muiTheme || muiStyles.getMuiTheme()
+    };
+
+    this.boundHandleStoreChange = this.handleStoreChange.bind(this);
+    this.debouncedWindowResize = debounce(100, this.windowResize.bind(this));
+  }
+
+  componentDidMount() {
+    ProjectStateStore.addChangeListener(this.boundHandleStoreChange);
+    ProjectStore.addChangeListener(this.boundHandleStoreChange);
+  }
+
+  componentWillUnmount() {
+    ProjectStateStore.removeChangeListener(this.boundHandleStoreChange);
+    ProjectStore.removeChangeListener(this.boundHandleStoreChange);
+  }
+
+  handleStoreChange() {
+    const { project, window } = this.props;
+    const projectState = ProjectStateStore.getProjectState(project);
+
+    this.setState({
+      isSelected: projectState.selection && projectState.selection.type === 'window' && projectState.selection.uid === window.uid,
+      lastUpdate: project.lastUpdate
+    });
+  }
+
+  windowResize(dir, size, rect, delta) {
+    window.height = size.height;
+    window.width = size.width;
+    ProjectActionCreators.refresh(this.props.project);
   }
 
   render() {
     const { window } = this.props;
+    const styles = getStyles(this.props, this.state);
 
     return (
       <div style={styles.container}>
-        <rr.ResizableBox width={window.width} height={window.height}>
-          window
-        </rr.ResizableBox>
+        <ResizableBox width={window.width}
+                      height={window.height}
+                      onResize={this.debouncedWindowResize}>
+          <div style={styles.window}>
+            window
+          </div>
+        </ResizableBox>
       </div>
     );
   }
 }
 
 CanvasWindow.propTypes = {
+  project: React.PropTypes.object.isRequired,
   window: React.PropTypes.object.isRequired,
+};
+
+CanvasWindow.contextTypes = {
+  muiTheme: React.PropTypes.object
+};
+
+CanvasWindow.childContextTypes = {
+  muiTheme: React.PropTypes.object
 };
 
 export default CanvasWindow;
