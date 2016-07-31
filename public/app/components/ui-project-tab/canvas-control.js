6b649574f53414b92776cfb524780812ab4a24fa
Vincent Trumpff <vincent@stoic.com>
2016-07-31 15:52:02 +0200

    canvas control

--- public/app/components/ui-project-tab/canvas-control.js
+++ public/app/components/ui-project-tab/canvas-control.js
@@ -7,31 +7,35 @@ import * as bs from 'react-bootstrap';
 import ResizableBox from 'react-resizable-box';
 import { throttle, debounce } from 'throttle-debounce';
 import base from '../base/index';
-import commonStyles from './canvas-styles';
-
-import CanvasControl from './canvas-control';
 
 import ProjectStore from '../../stores/project-store';
 import ProjectStateStore from '../../stores/project-state-store';
 import ProjectActionCreators from '../../actions/project-action-creators';
 
 function getStyles(props, state) {
+  const { window, control } = props;
   const { muiTheme, isSelected } = state;
   const { baseTheme } = muiTheme;
 
   const backColor = (isSelected ? baseTheme.palette.primary1Color : baseTheme.palette.primary3Color);
   const foreColor = (isSelected ? baseTheme.palette.alternateTextColor : baseTheme.palette.textColor);
+  const left = (window.width * control.x) - (control.width / 2);
+  const top = (window.height * control.y) - (control.height / 2);
+
+console.log(control);
 
   return Object.assign({
-    windowContainer: {
-      margin: '10px'
+    controlContainer: {
+      position : 'absolute',
+      left,
+      top
     },
-    window: {
+    control: {
       height: '100%',
       border: '1px solid ' + backColor,
       position: 'relative'
     },
-  }, commonStyles);
+  });
 }
 
 class CanvasWindow extends React.Component {
@@ -39,17 +43,17 @@ class CanvasWindow extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    const { project, window } = this.props;
+    const { project, control } = this.props;
     const projectState = ProjectStateStore.getProjectState(project);
 
     this.state = {
-      isSelected: projectState.selection && projectState.selection.type === 'window' && projectState.selection.uid === window.uid,
+      isSelected: projectState.selection && projectState.selection.type === 'control' && projectState.selection.uid === control.uid,
       lastUpdate: props.project.lastUpdate,
       muiTheme: context.muiTheme || muiStyles.getMuiTheme()
     };
 
     this.boundHandleStoreChange = this.handleStoreChange.bind(this);
-    this.debouncedWindowResize = debounce(100, this.windowResize.bind(this));
+    this.debouncedControlResize = debounce(100, this.controlResize.bind(this));
   }
 
   componentDidMount() {
@@ -63,50 +67,46 @@ class CanvasWindow extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    const { project, window } = nextProps;
+    const { project, control } = nextProps;
     const projectState = ProjectStateStore.getProjectState(project);
 
     this.setState({
-      isSelected: projectState.selection && projectState.selection.type === 'window' && projectState.selection.uid === window.uid,
+      isSelected: projectState.selection && projectState.selection.type === 'control' && projectState.selection.uid === control.uid,
       lastUpdate: project.lastUpdate
     });
   }
 
   handleStoreChange() {
-    const { project, window } = this.props;
+    const { project, control } = this.props;
     const projectState = ProjectStateStore.getProjectState(project);
 
     this.setState({
-      isSelected: projectState.selection && projectState.selection.type === 'window' && projectState.selection.uid === window.uid,
+      isSelected: projectState.selection && projectState.selection.type === 'control' && projectState.selection.uid === control.uid,
       lastUpdate: project.lastUpdate
     });
   }
 
-  windowResize(dir, size, rect, delta) {
-    const { project, window } = this.props;
+  controlResize(dir, size, rect, delta) {
+    const { project, control } = this.props;
 
-    window.height = size.height;
-    window.width = size.width;
+    control.height = size.height;
+    control.width = size.width;
     ProjectActionCreators.refresh(project);
   }
 
   render() {
-    const { project, window } = this.props;
+    const { control } = this.props;
     const styles = getStyles(this.props, this.state);
 
     return (
-      <div style={styles.container}>
-        <div style={styles.windowContainer}>
-          <ResizableBox width={window.width}
-                        height={window.height}
-                        onResize={this.debouncedWindowResize}>
-            <div style={styles.window}>
-              {/* TODO: background */}
-              {window.controls.map((ctrl) => (
-                <CanvasControl project={project} window={window} control={ctrl} />))}
-            </div>
-          </ResizableBox>
-        </div>
+      <div style={styles.controlContainer}>
+        <ResizableBox width={control.width}
+                      height={control.height}
+                      onResize={this.debouncedControlResize}>
+          <div style={styles.control}>
+            TODO
+          </div>
+        </ResizableBox>
       </div>
     );
   }
@@ -115,6 +115,7 @@ class CanvasWindow extends React.Component {
 CanvasWindow.propTypes = {
   project: React.PropTypes.object.isRequired,
   window: React.PropTypes.object.isRequired,
+  control: React.PropTypes.object.isRequired,
 };
 
 CanvasWindow.contextTypes = {
