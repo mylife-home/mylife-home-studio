7987579aad09e6b66768169ce2ddcca730d000a0
Vincent Trumpff <vincent@stoic.com>
2016-08-09 10:52:32 +0200

    import/deploy ui

--- public/app/components/ui-project-tab/toolbar.js
+++ public/app/components/ui-project-tab/toolbar.js
@@ -11,6 +11,8 @@ import ProjectStateStore from '../../stores/project-state-store';
 import ProjectActionCreators from '../../actions/project-action-creators';
 import DialogsActionCreators from '../../actions/dialogs-action-creators';
 
+import OnlineStore from '../../stores/online-store';
+
 import Facade from '../../services/facade';
 
 const styles = {
@@ -59,12 +61,14 @@ class Toolbar extends React.Component {
 
   // import
 
-  importProjectOnline() {
-    // TODO
+  importProjectFile() {
+    this.refs.openFileVPanelProject.click();
   }
 
-  importProjectFile() {
-    // TODO
+  importProjectOnline() {
+    this.setState({
+      openOnlineVPanelProjectItems: OnlineStore.getResourceNames('project.vpanel.').map(name => name.substring('project.vpanel.'.length))
+    });
   }
 
   importOnline() {
@@ -85,11 +89,89 @@ class Toolbar extends React.Component {
     });
   }
 
-  cancelImportOnlineToolbox() {
+  handleOpenFileVPanelProject(e) {
+    const file = e.target.files[0];
+    e.target.value = '';
+
+    const reader = new FileReader();
+
+    DialogsActionCreators.setBusy('Loading project');
+
+    reader.onloadend = () => {
+      DialogsActionCreators.unsetBusy();
+      const err = reader.error;
+      if(err) { return DialogsActionCreators.error(err); }
+      const content = reader.result;
+      let project;
+      try {
+        project = Facade.projects.open('vpanel', content, true);
+      } catch(err) {
+        return DialogsActionCreators.error(err);
+      }
+      return this.importProject(project);
+    };
+
+    reader.readAsText(file);
+  }
+
+  handleOpenOnlineVPanelProject(name) {
+    this.setState({
+      openOnlineVPanelProjectItems: null
+    });
+    if(!name) { return; }
+    this.loadProjectOnline('project.vpanel.' + name, 'vpanel');
+  }
+
+  loadProjectOnline(resource, type) {
+    const load = (content) => {
+      let project;
+      try {
+        project = Facade.projects.open(type, content, true);
+      } catch(err) {
+        return DialogsActionCreators.error(err);
+      }
+      return this.importProject(project);
+    }
+
+    const entity = OnlineStore.getResourceEntity();
+    const cachedContent = entity.cachedResources && entity.cachedResources[resource];
+    if(cachedContent) {
+      return load(cachedContent);
+    }
+
+    // need to get content .. TODO: Flux pattern to do that ?
+    DialogsActionCreators.setBusy('Loading project');
+    return ResourcesActionCreators.resourceGetQuery(entity.id, resource, (err, content) => {
+      DialogsActionCreators.unsetBusy();
+      if(err) { return DialogsActionCreators.error(err); }
+      return load(content);
+    });
+  }
+
+  importProject(vpanelProject) {
+    const project = this.props.project;
+    let data;
+    try {
+      data = Facade.projects.uiPrepareImportVpanelProject(project, vpanelProject);
+    } catch(err) {
+      return DialogsActionCreators.error(err);
+    }
+
+    if(data.messages && data.messages.length) {
+      this.setState({
+        importComponentsConfirm: data
+      });
+      return;
+    }
+
+    this.executeImportComponents(data);
+  }
+
+  cancelImportComponents() {
     this.setState({ importComponentsConfirm: null });
   }
 
-  confirmImportOnlineToolbox() {
+  confirmImportComponents() {
     const data = this.state.importComponentsConfirm;
     this.setState({ importComponentsConfirm: null });
     this.executeImportComponents(data);
@@ -216,6 +298,19 @@ class Toolbar extends React.Component {
                          open={!!this.state.showInfo}
                          lines={this.state.showInfo || []}
                          close={this.closeInfo.bind(this)}/>
+
+        <input
+          ref="openFileVPanelProject"
+          type="file"
+          style={{"display" : "none"}}
+          onChange={base.utils.stopPropagationWrapper(this.handleOpenFileVPanelProject.bind(this))}/>
+
+        <base.DialogSelect title="Select VPanel Project"
+                           open={!!this.state.openOnlineVPanelProjectItems}
+                           items={this.state.openOnlineVPanelProjectItems || []}
+                           select={this.handleOpenOnlineVPanelProject.bind(this)}
+                           cancel={this.handleOpenOnlineVPanelProject.bind(this, null)}/>
+
       </div>
     );
   }
