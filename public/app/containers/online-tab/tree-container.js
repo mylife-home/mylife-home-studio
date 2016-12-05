8e1d049ed70ef2f411bcca54eaaa625ff102860b
Vincent Trumpff <vincent@stoic.com>
2016-12-05 17:52:32 +0100

    tree-container to redux

--- public/app/containers/online-tab/tree-container.js
+++ public/app/containers/online-tab/tree-container.js
@@ -1,54 +1,17 @@
 'use strict';
 
-import React from 'react';
+import { connect } from 'react-redux';
 
 import Tree from '../../components/online-tab/tree';
 
-import OnlineStore from '../../stores/online-store';
-import storeHandler from '../../compat/store';
+const mapStateToProps = (state, ownProps) => ({
+  entities: state.online,
+  selectedValueChanged: ownProps.selectedValueChanged,
+  selectedNode: ownProps.selectedNode
+});
 
-const getAll = () => storeHandler.getStore().getState().online.toArray()
-
-class TreeContainer extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      entities: getAll()
-    };
-
-    this.boundHandleStoreChange = this.handleStoreChange.bind(this);
-  }
-
-  componentDidMount() {
-    this.unsubscribe = storeHandler.getStore().subscribe(this.boundHandleStoreChange);
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  handleStoreChange() {
-    this.setState({
-      entities: getAll()
-    });
-  }
-
-  render() {
-    const { selectedValueChanged, selectedNode } = this.props;
-    const { entities } = this.state;
-
-    return (<Tree
-      selectedValueChanged={selectedValueChanged}
-      selectedNode={selectedNode}
-      entities={entities}/>);
-  }
-}
-
-TreeContainer.propTypes = {
-  selectedValueChanged: React.PropTypes.func.isRequired,
-  selectedNode: React.PropTypes.object
-};
+const TreeContainer = connect(
+  mapStateToProps
+)(Tree);
 
 export default TreeContainer;
