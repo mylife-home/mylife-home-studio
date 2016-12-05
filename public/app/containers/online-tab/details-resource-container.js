1a42d2ae7aaeaae2565418baf21209a69541f5dc
Vincent Trumpff <vincent@stoic.com>
2016-12-05 20:33:27 +0100

    details resource container to redux

--- public/app/containers/online-tab/details-resource-container.js
+++ public/app/containers/online-tab/details-resource-container.js
@@ -1,63 +1,22 @@
 'use strict';
 
-import React from 'react';
+import { connect } from 'react-redux';
 
 import DetailsResource from '../../components/online-tab/details-resource';
-
-import storeHandler from '../../compat/store';
-
 import { resourcesGetQuery } from '../../actions/index';
 
-class DetailsResourceContainer extends React.Component {
+const mapStateToProps = (state, { entity, resource }) => ({
+  content: entity.cachedResources && entity.cachedResources[resource],
+  resource
+});
 
-  constructor(props) {
-    super(props);
+const mapDispatchToProps = (dispatch, { entity, resource }) => ({
+  onRefresh: () => resourcesGetQuery(entity.id, resource) // FIXME: dispatch
+});
 
-    this.state = {
-      content: null
-    };
-
-    this.boundHandleStoreChange = this.handleStoreChange.bind(this);
-  }
-
-  componentDidMount() {
-    this.unsubscribe = storeHandler.getStore().subscribe(this.boundHandleStoreChange);
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const entity = nextProps.entity || this.props.entity;
-    const resource = nextProps.resource || this.props.resource;
-
-    this.setState({
-      content: entity.cachedResources && entity.cachedResources[resource]
-    });
-  }
-
-  handleStoreChange() {
-    const entity = this.props.entity;
-    const resource = this.props.resource;
-
-    this.setState({
-      content: entity.cachedResources && entity.cachedResources[resource]
-    });
-  }
-
-  render() {
-    const { entity, resource } = this.props;
-    const { content } = this.state;
-    const refreshAction = () => resourcesGetQuery(entity.id, resource);
-
-    return (<DetailsResource resource={resource} content={content} onRefresh={refreshAction} />);
-  }
-}
-
-DetailsResourceContainer.propTypes = {
-  entity: React.PropTypes.object.isRequired,
-  resource: React.PropTypes.string.isRequired,
-};
+const DetailsResourceContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DetailsResource);
 
 export default DetailsResourceContainer;
