d1267c83f6ab483ab85551065cc052973ca7e877
Vincent Trumpff <vincent@stoic.com>
2016-12-05 09:49:18 +0100

    main-toolbar refactoring

--- public/app/components/main-toolbar.js
+++ public/app/components/main-toolbar.js
@@ -16,88 +16,82 @@ const styles = {
   }
 };
 
-class MainToolbar extends React.Component {
+const MainToolbar = ({
+  muiTheme, activeProject, onlineVPanelProjectList, onlineUiProjectList,
+  onNewVPanelProject, onNewUiProject, onOpenFileVPanelProject, onOpenFileUiProject, onOpenOnlineVPanelProject, onOpenOnlineUiProject, onSaveAll, onSaveOnline, onSaveAs }) => {
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  const iconStyle = Object.assign({}, styles.icon, { fill: muiTheme.toolbar.iconColor});
 
-  render() {
-    const iconStyle = Object.assign({}, styles.icon, { fill: this.props.muiTheme.toolbar.iconColor});
-    const project = this.props.activeProject;
+  return (
+    <mui.Toolbar>
+      <mui.ToolbarGroup>
+        <base.icons.tabs.VPanel style={iconStyle} />
+        <mui.ToolbarTitle text="vpanel" />
 
-    return (
-      <mui.Toolbar>
-        <mui.ToolbarGroup>
-          <base.icons.tabs.VPanel style={iconStyle} />
-          <mui.ToolbarTitle text="vpanel" />
+        <mui.IconButton tooltip="new"
+                        style={styles.button}
+                        onClick={() => onNewVPanelProject()}>
+          <base.icons.actions.New />
+        </mui.IconButton>
+        <base.IconSelectButton tooltip="open online"
+                              style={styles.button}
+                              selectTitle={"Select VPanel Project"}
+                              selectItems={onlineVPanelProjectList}
+                              onItemSelect={onOpenOnlineVPanelProject}>
+          <base.icons.actions.OpenOnline />
+        </base.IconSelectButton>
+        <base.IconFileButton tooltip="open from file"
+                             style={styles.button}
+                             onFileSelected={onOpenFileVPanelProject}>
+          <base.icons.actions.OpenFile />
+        </base.IconFileButton>
 
-          <mui.IconButton tooltip="new"
-                          style={styles.button}
-                          onClick={() => this.props.onNewVPanelProject()}>
-            <base.icons.actions.New />
-          </mui.IconButton>
-          <base.IconSelectButton tooltip="open online"
-                                style={styles.button}
-                                selectTitle={"Select VPanel Project"}
-                                selectItems={this.props.onlineVPanelProjectList}
-                                onItemSelect={this.props.onOpenOnlineVPanelProject}>
-            <base.icons.actions.OpenOnline />
-          </base.IconSelectButton>
-          <base.IconFileButton tooltip="open from file"
-                               style={styles.button}
-                               onFileSelected={this.props.onOpenFileVPanelProject}>
-            <base.icons.actions.OpenFile />
-          </base.IconFileButton>
+        <mui.ToolbarSeparator />
 
-          <mui.ToolbarSeparator />
+        <base.icons.tabs.Ui style={iconStyle} />
+        <mui.ToolbarTitle text="ui"/>
 
-          <base.icons.tabs.Ui style={iconStyle} />
-          <mui.ToolbarTitle text="ui"/>
+        <mui.IconButton tooltip="new"
+                        style={styles.button}
+                        onClick={() => onNewUiProject()}>
+          <base.icons.actions.New />
+        </mui.IconButton>
+        <base.IconSelectButton tooltip="open online"
+                              style={styles.button}
+                              selectTitle={"Select UI Project"}
+                              selectItems={onlineUiProjectList}
+                              onItemSelect={onOpenOnlineUiProject}>
+          <base.icons.actions.OpenOnline />
+        </base.IconSelectButton>
+        <base.IconFileButton tooltip="open from file"
+                             style={styles.button}
+                             onFileSelected={onOpenFileUiProject}>
+          <base.icons.actions.OpenFile />
+        </base.IconFileButton>
 
-          <mui.IconButton tooltip="new"
-                          style={styles.button}
-                          onClick={() => this.props.onNewUiProject()}>
-            <base.icons.actions.New />
-          </mui.IconButton>
-          <base.IconSelectButton tooltip="open online"
-                                style={styles.button}
-                                selectTitle={"Select UI Project"}
-                                selectItems={this.props.onlineUiProjectList}
-                                onItemSelect={this.props.onOpenOnlineUiProject}>
-            <base.icons.actions.OpenOnline />
-          </base.IconSelectButton>
-          <base.IconFileButton tooltip="open from file"
-                               style={styles.button}
-                               onFileSelected={this.props.onOpenFileUiProject}>
-            <base.icons.actions.OpenFile />
-          </base.IconFileButton>
+        <mui.ToolbarSeparator />
 
-          <mui.ToolbarSeparator />
-
-          <mui.IconButton tooltip="save all"
-                          style={styles.button}
-                          onClick={() => this.props.onSaveAll()}>
-            <base.icons.actions.SaveAll />
-          </mui.IconButton>
-          <mui.IconButton tooltip="save online"
-                          style={styles.button}
-                          disabled={!project}
-                          onClick={() => this.props.onSaveOnline(this.props.activeProject)}>
-            <base.icons.actions.Save />
-          </mui.IconButton>
-          <mui.IconButton tooltip="save as"
-                          style={styles.button}
-                          disabled={!project}
-                          onClick={() => this.props.onSaveAs(this.props.activeProject)}>
-            <base.icons.actions.SaveAs />
-          </mui.IconButton>
-        </mui.ToolbarGroup>
-      </mui.Toolbar>
-    );
-  }
-}
+        <mui.IconButton tooltip="save all"
+                        style={styles.button}
+                        onClick={() => onSaveAll()}>
+          <base.icons.actions.SaveAll />
+        </mui.IconButton>
+        <mui.IconButton tooltip="save online"
+                        style={styles.button}
+                        disabled={!activeProject}
+                        onClick={() => onSaveOnline(activeProject)}>
+          <base.icons.actions.Save />
+        </mui.IconButton>
+        <mui.IconButton tooltip="save as"
+                        style={styles.button}
+                        disabled={!activeProject}
+                        onClick={() => onSaveAs(activeProject)}>
+          <base.icons.actions.SaveAs />
+        </mui.IconButton>
+      </mui.ToolbarGroup>
+    </mui.Toolbar>
+  );
+};
 
 MainToolbar.propTypes = {
   activeProject: React.PropTypes.object,
