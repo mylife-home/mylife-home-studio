1d9711ef79ffe9b0e15ff51220b6961b550e5e36
Vincent Trumpff <vincent@stoic.com>
2016-12-18 23:39:37 +0100

    cleanup

--- public/app/compat/dispatcher.js
+++ public/app/compat/dispatcher.js
@@ -1,18 +1,11 @@
 'use strict';
 
-import { Dispatcher } from 'flux';
 import store from './store';
-
-const fluxDispatcher = new Dispatcher();
 
 export default {
   dispatch: (action) => {
     if(!action) { return; }
     return store.getStore().dispatch(action);
-  },
-
-  register: (handler) => {
-    return fluxDispatcher.register(handler);
-  },
+  }
 };
 
