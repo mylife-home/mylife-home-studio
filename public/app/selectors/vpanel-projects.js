0f7a80c1e9fc47d6c339c7f1674516f5ec5cd57d
Vincent Trumpff <vincent@stoic.com>
2016-12-19 17:23:39 +0100

    vpanel selector skeleton

---
+++ public/app/selectors/vpanel-projects.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//import { createSelector } from 'reselect';
+import { getProject } from './projects';
+
+export const getComponents = (state, props) => getProject(state, props).components;
+export const getPlugins    = (state, props) => getProject(state, props).plugins;
+export const getBindings   = (state, props) => getProject(state, props).bindings;
+
+export const getComponent  = (state, props) => getComponents(state, props).get(props.component);
+export const getPlugin     = (state, props) => getPlugins(state, props).get(props.plugin);
+export const getBinding    = (state, props) => getBindings(state, props).get(props.binding);
