a0c7515fdefd1267bb90ccb4b0da6a38427e9d5f
Vincent Trumpff <vincent@stoic.com>
2016-12-26 20:21:03 +0100

    index cleanup

--- public/app/components/vpanel-project-tab/index.js
+++ public/app/components/vpanel-project-tab/index.js
@@ -15,46 +15,35 @@ import { projectClose } from '../../actions/index';
 
 import tabStyles from '../base/tab-styles';
 
-class VPanelProjectTab extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { project } = this.props;
-
-    return (
-      <bs.Grid fluid={true} style={Object.assign({}, tabStyles.fullHeight)}>
-        <bs.Row style={tabStyles.fullHeight}>
-          <bs.Col sm={2} style={Object.assign({}, tabStyles.noPadding, tabStyles.fullHeight)}>
-            <mui.Paper style={Object.assign({}, tabStyles.scrollable, tabStyles.fullHeight)}>
-              <ToolboxContainer project={project.uid} />
-            </mui.Paper>
-          </bs.Col>
-          <bs.Col sm={8} style={Object.assign({}, tabStyles.noPadding, tabStyles.scrollable, tabStyles.fullHeight)}>
-            <div style={Object.assign({marginTop: '-10px' /* WTF ?! */}, tabStyles.noPadding, tabStyles.fullHeight)}>
-              <MainTitle
-                center={project.name}
-                left={<icons.tabs.VPanel />}
-                right={
-                  <mui.IconButton onClick={() => AppDispatcher.dispatch(projectClose(project.uid))}>
-                    <icons.actions.Close />
-                  </mui.IconButton>
-                }/>
-              <Canvas project={project} />
-            </div>
-          </bs.Col>
-          <bs.Col sm={2} style={Object.assign({}, tabStyles.noPadding, tabStyles.fullHeight)}>
-            <mui.Paper style={Object.assign({}, tabStyles.scrollable, tabStyles.fullHeight)}>
-              <Properties project={project} />
-            </mui.Paper>
-          </bs.Col>
-        </bs.Row>
-      </bs.Grid>
-    );
-  }
-}
+const VPanelProjectTab = ({ project }) => (
+  <bs.Grid fluid={true} style={Object.assign({}, tabStyles.fullHeight)}>
+    <bs.Row style={tabStyles.fullHeight}>
+      <bs.Col sm={2} style={Object.assign({}, tabStyles.noPadding, tabStyles.fullHeight)}>
+        <mui.Paper style={Object.assign({}, tabStyles.scrollable, tabStyles.fullHeight)}>
+          <ToolboxContainer project={project.uid} />
+        </mui.Paper>
+      </bs.Col>
+      <bs.Col sm={8} style={Object.assign({}, tabStyles.noPadding, tabStyles.scrollable, tabStyles.fullHeight)}>
+        <div style={Object.assign({marginTop: '-10px' /* WTF ?! */}, tabStyles.noPadding, tabStyles.fullHeight)}>
+          <MainTitle
+            center={project.name}
+            left={<icons.tabs.VPanel />}
+            right={
+              <mui.IconButton onClick={() => AppDispatcher.dispatch(projectClose(project.uid))}>
+                <icons.actions.Close />
+              </mui.IconButton>
+            }/>
+          <Canvas project={project} />
+        </div>
+      </bs.Col>
+      <bs.Col sm={2} style={Object.assign({}, tabStyles.noPadding, tabStyles.fullHeight)}>
+        <mui.Paper style={Object.assign({}, tabStyles.scrollable, tabStyles.fullHeight)}>
+          <Properties project={project} />
+        </mui.Paper>
+      </bs.Col>
+    </bs.Row>
+  </bs.Grid>
+);
 
 VPanelProjectTab.propTypes = {
   project: React.PropTypes.object.isRequired,
