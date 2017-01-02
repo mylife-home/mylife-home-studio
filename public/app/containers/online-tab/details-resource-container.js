352cd3341f50e3426c3795cc8bb29896dce5e8cb
Vincent Trumpff <vincent@stoic.com>
2017-01-02 09:48:34 +0100

    cleanup

--- public/app/containers/online-tab/details-resource-container.js
+++ public/app/containers/online-tab/details-resource-container.js
@@ -3,7 +3,6 @@
 import { connect } from 'react-redux';
 
 import DetailsResource from '../../components/online-tab/details-resource';
-import AppDispatcher from '../../compat/dispatcher';
 import { resourcesGet } from '../../actions/index';
 
 const mapStateToProps = (state, { entity, resource }) => ({
@@ -12,7 +11,7 @@ const mapStateToProps = (state, { entity, resource }) => ({
 });
 
 const mapDispatchToProps = (dispatch, { entity, resource }) => ({
-  onRefresh: () => AppDispatcher.dispatch(resourcesGet(entity.id, resource))
+  onRefresh: () => dispatch(resourcesGet(entity.id, resource))
 });
 
 const DetailsResourceContainer = connect(
