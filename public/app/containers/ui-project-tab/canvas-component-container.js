133e22c22e0371fa7fdfb86218480763bc86f5da
Vincent Trumpff <vincent@stoic.com>
2017-01-04 10:37:55 +0100

    cleanup

--- public/app/containers/ui-project-tab/canvas-component-container.js
+++ public/app/containers/ui-project-tab/canvas-component-container.js
@@ -6,11 +6,9 @@ import CanvasComponent from '../../components/ui-project-tab/canvas-component';
 
 import { getComponent } from '../../selectors/ui-projects';
 
-const mapStateToProps = (state, { project, component }) => {
-  return {
-    component : getComponent(state, { project, component })
-  };
-};
+const mapStateToProps = (state, { project, component }) => ({
+  component : getComponent(state, { project, component })
+});
 
 const CanvasComponentContainer = connect(
   mapStateToProps,
