79ec4a9f01414b2c8c7638452f2266eb0d74457c
Vincent Trumpff <vincent@stoic.com>
2017-07-04 10:58:27 +0200

    format network overview properly

--- public/app/components/online-tab/details-network.js
+++ public/app/components/online-tab/details-network.js
@@ -5,36 +5,30 @@ import * as mui from 'material-ui';
 import icons from '../icons';
 import MainTitle from '../main-title';
 import humanFormat from 'human-format';
+import moment from 'moment';
+import 'moment-duration-format';
 
 import DetailsContainer from './details-container';
 
-const timeScale = new humanFormat.Scale({
-  seconds : 1,
-  minutes : 60,
-  hours   : 3600,
-  days    : 86400,
-  months  : 2592000,
-});
-
-const timeFormat = t => humanFormat(t, { scale: timeScale });
-const memFormat = m => humanFormat(m, { scale: 'binary', unit: 'B' });
+const timeFormat      = t => moment.duration(t).humanize();
+const smallTimeFormat = t => moment.duration(t).format();
+const memFormat       = m => humanFormat(m, { scale: 'binary', unit: 'B' });
 
 function renderHeader() {
   return (
-    <mui.Paper>
+    <mui.Paper style={{ width: 3000 }}>
       <div style={{ display: 'flex', flexDirection: 'row', textAlign: 'left', padding: 16, fontWeight: 'bold' }}>
         <div style={{ flex: 2 }}>Id</div>
-        <div style={{ flex: 1 }}>OS.Platform</div>
-        <div style={{ flex: 1 }}>OS.Cpus</div>
-        <div style={{ flex: 1 }}>OS.LoadAverage</div>
-        <div style={{ flex: 1 }}>OS.Memory</div>
-        <div style={{ flex: 1 }}>OS.Uptime</div>
-        <div style={{ flex: 1 }}>OS.Uptime</div>
-        <div style={{ flex: 1 }}>Process.NodeVersion</div>
-        <div style={{ flex: 1 }}>Process.AppVersion</div>
-        <div style={{ flex: 1 }}>Process.CommonVersion</div>
-        <div style={{ flex: 1 }}>Process.CpuUsage</div>
-        <div style={{ flex: 1 }}>Process.Uptime</div>
+        <div style={{ flex: 1 }}>OS platform</div>
+        <div style={{ flex: 2 }}>OS cpus</div>
+        <div style={{ flex: 1 }}>OS load average (1, 5, 15min)</div>
+        <div style={{ flex: 1 }}>OS memory (free / total)</div>
+        <div style={{ flex: 1 }}>OS uptime</div>
+        <div style={{ flex: 1 }}>Process node version</div>
+        <div style={{ flex: 1 }}>Process app version</div>
+        <div style={{ flex: 1 }}>Process common version</div>
+        <div style={{ flex: 1 }}>Process cpu usage</div>
+        <div style={{ flex: 1 }}>Process uptime</div>
       </div>
     </mui.Paper>
   );
@@ -64,15 +58,15 @@ function renderEntity(entity) {
         <div style={{ display: 'flex', flexDirection: 'row', textAlign: 'left' }}>
           <div style={{ flex: 2 }}>{entity.id}</div>
           <div style={{ flex: 1 }}>{systemCheck(s => `${s['os.arch']}/${s['os.platform']} - ${s['os.release']}`)}</div>
-          <div style={{ flex: 1 }}>{systemCheck(s => JSON.stringify(s['os.cpus']))}</div>
+          <div style={{ flex: 2 }}>{systemCheck(s => s['os.cpus'].map(cpu => `${cpu.model} @${cpu.speed}MHz x${cpu.count}`).join('\n'))}</div>
           <div style={{ flex: 1 }}>{systemCheck(s => s['os.loadavg'].map(round2dec).join(', '))}</div>
-          <div style={{ flex: 1 }}>{systemCheck(s => `${memFormat(s['os.freemem'])} free / ${memFormat(s['os.totalmem'])} total`)}</div>
-          <div style={{ flex: 1 }}>{systemCheck(s => timeFormat(s['os.uptime']))}</div>
+          <div style={{ flex: 1 }}>{systemCheck(s => `${memFormat(s['os.freemem'])} / ${memFormat(s['os.totalmem'])}`)}</div>
+          <div style={{ flex: 1 }}>{systemCheck(s => timeFormat(s['os.uptime'] * 1000))}</div>
           <div style={{ flex: 1 }}>{systemCheck(s => s['process.version'])}</div>
           <div style={{ flex: 1 }}>{appPackage && appPackage.version}</div>
           <div style={{ flex: 1 }}>{commonPackage && commonPackage.version}</div>
-          <div style={{ flex: 1 }}>{systemCheck(s => `user: ${timeFormat(s['process.cpuUsage'].user / 1E6)}, system: ${timeFormat(s['process.cpuUsage'].system / 1E6)}`)}</div>
-          <div style={{ flex: 1 }}>{systemCheck(s => timeFormat(s['process.uptime']))}</div>
+          <div style={{ flex: 1 }}>{systemCheck(s => `user: ${smallTimeFormat(s['process.cpuUsage'].user / 1000)}, system: ${smallTimeFormat(s['process.cpuUsage'].system / 1000)}`)}</div>
+          <div style={{ flex: 1 }}>{systemCheck(s => timeFormat(s['process.uptime'] * 1000))}</div>
         </div>
       }/>
   );
@@ -96,7 +90,7 @@ const DetailsNetwork = ({ entities, onRefresh }) => (
     <DetailsContainer>
       <div>
         {renderHeader()}
-        <mui.List style={{overflowX:'hidden'}}>
+        <mui.List style={{ overflowX:'hidden', width: 3000 }}>
           {entities.map(renderEntity)}
         </mui.List>
       </div>
